045b71f333eda3d1635594bff0b2fa544d740591
Nir <hl010157@163.com>
2024-10-09 12:35:16 +0800

    Note

--- LudumDare_45/Assets/Scripts/SpeedLineBehaviour.cs
+++ LudumDare_45/Assets/Scripts/SpeedLineBehaviour.cs
@@ -19,11 +19,15 @@ public class SpeedLineBehaviour : MonoBehaviour
     
     void OnCollisionStay2D(Collision2D collisionInfo){
         if(collisionInfo.gameObject.tag == "Player"){
-            Vector2 direction = Rotate(collisionInfo.contacts[0].normal, 90);
+            //Vector2 direction = Rotate(collisionInfo.contacts[0].normal, 90);
+            Vector2 normal = collisionInfo.contacts[0].normal;
+            Vector2 direction = new Vector2(-normal.y, normal.x);//就是得到切线，可以代替Rotate方法
+            //print($"Rotate(collisionInfo.contacts[0].normal, 90);   {Rotate(collisionInfo.contacts[0].normal, 90)}");
+            //print($"direction  {direction}");
             collisionInfo.gameObject.GetComponent<BallBehaviour>().AddForce(direction.normalized*speedPower);
         }
     }
-
+    //矩阵运算得到旋转后的向量
     private Vector2 Rotate(Vector2 v, float degrees) {
          float sin = Mathf.Sin(degrees * Mathf.Deg2Rad);
          float cos = Mathf.Cos(degrees * Mathf.Deg2Rad);
